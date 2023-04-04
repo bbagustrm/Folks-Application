5f1cb1b977371fa9dac06d1ba5c72e11e7a9b44e
bbagustrm <bbagustrm@gmail.com>
2023-04-04 15:26:21 +0700

    adding splash screen and login form

---
+++ app/src/main/java/com/example/folksapplication/FormLoginActivity.kt
@@ -0,0 +1,11 @@
+package com.example.folksapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FormLoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form_login)
+    }
+}
