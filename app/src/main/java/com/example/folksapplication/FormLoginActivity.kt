7d94bd7ac2621dda2a27cdf082b41a52cae2c642
bbagustrm <bbagustrm@gmail.com>
2023-04-04 20:37:50 +0700

    Finished from login and regist

--- app/src/main/java/com/example/folksapplication/FormLoginActivity.kt
+++ app/src/main/java/com/example/folksapplication/FormLoginActivity.kt
@@ -1,11 +1,23 @@
 package com.example.folksapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class FormLoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_form_login)
+
+        val registerForm = findViewById<Button>(R.id.registButton)
+
+        registerForm.setOnClickListener{
+            Intent(this, FormRegistActivity::class.java). also{
+                startActivity(it)
+            }
+        }
+
+
     }
 }
